8921a8794a4e1c0ea37d00333d9a7a47e3e46460
Sarp <sarp.ozturk@ozu.edu.tr>
2020-03-04 21:19:46 +0300

    first commit

---
+++ ToDoApp/ChecklistViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by Sarp Ozturk on 2.03.2020.
+//  Copyright © 2020 Sarp Ozturk. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        return cell
+    }
+
+}
+
